2eededc7a0cec1d984603cc5b614cb7690b44b4a
Dhruvan <navrudh@gmail.com>
2021-06-13 13:27:02 +0200

    add exit cond to helper scripts

--- helper-scripts/get-data.sh
+++ helper-scripts/get-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-cd "$SCRIPTS_DIR/../data"
+cd "$SCRIPTS_DIR/../data" || exit
 wget -nc https://archive.ics.uci.edu/ml/machine-learning-databases/00339/train.csv.zip
 7z x "train.csv.zip" -aos
