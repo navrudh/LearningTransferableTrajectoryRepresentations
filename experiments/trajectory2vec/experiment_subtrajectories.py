df718c76b4c061e32d35425ec38f4387597540f3
Dhruvan Ganesh <navrudh@gmail.com>
2021-09-05 14:18:45 +0200

    add more info, improve perf of rank query

--- experiments/trajectory2vec/experiment_subtrajectories.py
+++ experiments/trajectory2vec/experiment_subtrajectories.py
@@ -18,15 +18,21 @@ def subtrajectory_experiment(query_results_file: str, query_database_results_fil
     query_database = pandas.DataFrame(query_database_results, columns=["TRAJECTORY_ID", "VECTOR"])
 
     ranks = []
-    for _, (query_id, query_vector) in enumerate(tqdm(query_results, desc=f"searching query (dbsize={db_size})")):
+    for _, (query_id, query_vector) in enumerate(
+            tqdm(query_results, desc=f"searching query (m= {m}, dbsize={len(query_database_results)})")
+    ):
         query_database['DISTANCE'] = query_database["VECTOR"].transform(lambda vec: sqeuclidean(query_vector, vec))
         sorted_database = query_database.sort_values(by=['DISTANCE'])
+
+        # ranks.append(sorted_database[sorted_database['TRAJECTORY_ID'] == query_id].index.values[0] + 1)
         for (idx, trajectory_id, _, _) in sorted_database.itertuples():
             if trajectory_id == query_id:
                 ranks.append(idx + 1)
+                break
 
     mean_rank = np.mean(ranks)
     print("Database Len:", len(query_database))
+    print("M:", m)
     print("Mean Rank:", mean_rank)
 
 
