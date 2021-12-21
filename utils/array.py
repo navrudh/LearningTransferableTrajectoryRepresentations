27bb81fd5fb3de78ef3476646fa62b2208bfb9c0
Dhruvan Ganesh <navrudh@gmail.com>
2021-12-21 17:29:59 +0100

    add safe and unsafe version of downsampling scripts

--- utils/array.py
+++ utils/array.py
@@ -5,6 +5,13 @@ def downsampling(arr: np.array, rate: float):
     if rate == 0.0:
         return arr
     keep = np.random.random_sample(size=arr.shape[0])
+    return arr[keep > rate]
+
+
+def downsampling_safe(arr: np.array, rate: float):
+    if rate == 0.0:
+        return arr
+    keep = np.random.random_sample(size=arr.shape[0])
     keep[0] = 1
     keep[-1] = 1
     return arr[keep > rate]
